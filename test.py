61f92122f95c2d974f06544951e4149c13e267a3
Sevki Yontem <sevki@fireflyon.com>
2021-12-09 00:31:03 +0300

    data hazir, video sonu

--- test.py
+++ test.py
@@ -5,21 +5,24 @@ from main import TurkishGekkoProphetService
 from utils import *
 
 
+def belli_aralik_icin_input_verisi_olustur(baslangic_gunu, bitis_gunu):
+    while baslangic_gunu <= bitis_gunu:
+        file_name = gune_ait_dosya_adi_olustur(coin, datetime.strftime(baslangic_gunu, '%Y_%m_%d'))
+        df = gunluk_satir_olustur(coin, baslangic_gunu)
+
+        print(f'{baslangic_gunu} icin satir hazirlandi!!')
+        baslangic_gunu = baslangic_gunu + timedelta(days=1)
+        df.to_csv(file_name, index=False)
+
+
+def gunluk_satir_olustur(coin, today):
+    return prophet_service.get_prophet_row(coin, today)
+
+
 if __name__ == '__main__':
     coin = 'ETHUSDT'
     config = {'API_KEY': API_KEY, 'API_SECRET': API_SECRET}
-
     prophet_service = TurkishGekkoProphetService(config)
-    today = date.today()
-    file_name = gune_ait_dosya_adi_olustur(coin, datetime.strftime('%Y_%m_%d'))
-    i = 0
-    df_list = []
-    while i < 14:
-        df_list.append(prophet_service.get_prophet_row(coin, today))
-        print(f'{today} icin satir hazirlandi!!')
-        today = today - timedelta(days=1)
-        i = i + 1
-    df = pd.concat(df_list)
-    # tempnameB = './coindata/example.csv'
-    df.to_csv(file_name, index=False)
-    print(df)
+    baslangic_gunu = datetime.strptime('2021-01-06', '%Y-%m-%d')
+    bitis_gunu = datetime.strptime('2021-12-07', '%Y-%m-%d')
+    belli_aralik_icin_input_verisi_olustur(baslangic_gunu, bitis_gunu)
